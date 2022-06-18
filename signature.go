a5bfa9c04e86d7a81dcda064b7f7892a4f3476ce
Kerry Su <me@sshockwave.net>
2022-06-18 13:47:08 +0800

    Feat(signature) use base58 for marshaling

--- signature.go
+++ signature.go
@@ -3,12 +3,14 @@ package main
 import (
 	"crypto/dsa"
 	"crypto/rand"
+	"encoding/asn1"
 	"fmt"
 	"log"
 	"math/big"
 	"strconv"
 	"strings"
 
+	"github.com/mr-tron/base58"
 	"github.com/sshockwave/bitebi/message"
 	"github.com/sshockwave/bitebi/utils"
 )
@@ -90,83 +92,32 @@ func FindAccountFromPkScript(txType string, pk_script []byte) (pks []dsa.PublicK
 	return pks
 }
 
-func Parameters2Bytes(parameters dsa.Parameters) (b []byte) {
-	p := *parameters.P
-	q := *parameters.Q
-	g := *parameters.G
-
-	var result string = p.String() + "$" + q.String() + "$" + g.String()
-	return []byte(result)
-}
-
 func PK2Bytes(key dsa.PublicKey) (b []byte) {
-	params := key.Parameters
-	part1 := Parameters2Bytes(params)
-	y := *key.Y
-	var result string = string(part1) + "$" + y.String()
-	return []byte(result)
+	mar, _ := asn1.Marshal(key)
+	return []byte(base58.Encode(mar))
 }
 
 func SK2Bytes(key dsa.PrivateKey) (b []byte) {
-	pk := key.PublicKey
-	part1 := PK2Bytes(pk)
-	x := *key.X
-	var result string = string(part1) + "$" + x.String()
-	return []byte(result)
-}
-
-func Bytes2Parameters(b []byte) dsa.Parameters {
-	param_bytes := string(b)
-	param := strings.FieldsFunc(param_bytes, splitKeys)
-	p := param[0]
-	q := param[1]
-	g := param[2]
-
-	big_p, _ := new(big.Int).SetString(p, 10)
-	big_q, _ := new(big.Int).SetString(q, 10)
-	big_g, _ := new(big.Int).SetString(g, 10)
-
-	return dsa.Parameters{P: big_p, Q: big_q, G: big_g}
+	mar, _ := asn1.Marshal(key)
+	return []byte(base58.Encode(mar))
 }
 
 func Bytes2PK(b []byte) (pub dsa.PublicKey) {
-	pk_bytes := string(b)
-	param := strings.FieldsFunc(pk_bytes, splitKeys)
-	if len(param) < 4 {
+	asn, err := base58.Decode(string(b))
+	if err != nil {
 		return
 	}
-	p := param[0]
-	q := param[1]
-	g := param[2]
-	y := param[3]
-
-	big_p, _ := new(big.Int).SetString(p, 10)
-	big_q, _ := new(big.Int).SetString(q, 10)
-	big_g, _ := new(big.Int).SetString(g, 10)
-	big_y, _ := new(big.Int).SetString(y, 10)
-
-	par := dsa.Parameters{P: big_p, Q: big_q, G: big_g}
-	return dsa.PublicKey{Parameters: par, Y: big_y}
+	_, err = asn1.Unmarshal(asn, &pub)
+	return
 }
 
-func Bytes2SK(b []byte) dsa.PrivateKey {
-	sk_bytes := string(b)
-	param := strings.FieldsFunc(sk_bytes, splitKeys)
-	p := param[0]
-	q := param[1]
-	g := param[2]
-	y := param[3]
-	x := param[4]
-
-	big_p, _ := new(big.Int).SetString(p, 10)
-	big_q, _ := new(big.Int).SetString(q, 10)
-	big_g, _ := new(big.Int).SetString(g, 10)
-	big_y, _ := new(big.Int).SetString(y, 10)
-	big_x, _ := new(big.Int).SetString(x, 10)
-
-	par := dsa.Parameters{P: big_p, Q: big_q, G: big_g}
-	pk := dsa.PublicKey{Parameters: par, Y: big_y}
-	return dsa.PrivateKey{PublicKey: pk, X: big_x}
+func Bytes2SK(b []byte) (prv dsa.PrivateKey) {
+	asn, err := base58.Decode(string(b))
+	if err != nil {
+		return
+	}
+	_, err = asn1.Unmarshal(asn, &prv)
+	return
 }
 
 func splitSignatures(s rune) bool {
